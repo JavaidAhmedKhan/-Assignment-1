73d8c2f1e589ff9edf8ca9d3db5e97d6acc5cf2b
JavaidAhmedKhan <141310014+JavaidAhmedKhan@users.noreply.github.com>
2023-08-12 13:26:20 +0500

    Add files via upload
    NED Assignment 1 to 5

---
+++ NED.py
@@ -0,0 +1,47 @@
+# # Assignment # 1
+
+# name = 'Javaid Ahmed Khan'
+# aga = '35'
+# country = 'Pakistan'
+# print('my name is ' + name, 'I am ' + aga, 'I live in ' + country)
+
+
+# Assignment # 2
+
+# item1 = 'Laptop' 
+# item2 = 'Book' 
+# item3 = 'Phone'
+# price1 = 5000
+# price2 = 1000
+# price3 = 4000
+# quantity = 4
+# total_cost = price1 * quantity
+# output = 'I bought {}, {}s at {} each for a total of $ {}'.format(quantity,item1, price1, total_cost) 
+# print(output)
+
+# Assignment # 3
+
+# city = 'Karachi'
+# temperature = '35'
+# output = f'the temerature of {city} is {temperature}'
+# print(output)
+
+# Assignment # 4
+
+# first_name = 'Javaid'
+# last_name = 'Khan'
+# birth_year = 1987
+# current_year = 2023
+# age = current_year - birth_year
+# output = 'My name is %s %s. I am %s years old ' %(first_name, last_name, age)
+# print(output)
+
+# Assignment # 5
+
+product_name = 'head & Shoulder'
+discount = 10
+original_price = 1000
+discounted_price = original_price - (original_price * discount/100)  
+output = f"{product_name}'s orignal price is {original_price}, whereas after {discount} discount. 'the final price is {discounted_price}"
+print(output)
+ 
